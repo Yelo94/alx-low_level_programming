3a57e6e4f8cbf145795d06800b30be45ec3aff87
yelo94 <akpamib@yahoo.com>
2022-07-02 19:35:45 -0700

    /**  * main -entry point  * return: always 0 (success)  */ int main(void) { char a; int i; long int li; long long int lli; float f;
    printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
    
    printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
    
    printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
    
    printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
    
     printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
    
    return (0)
    }
    
    git commit -t 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,26 +4,20 @@
  * return: always 0 (success)
  */
 int main(void)
-
 {
-
-int a;
-
-long int b;
-
-long long int c;
-
-char d;
-
+char a;
+int i;
+long int li;
+long long int lli;
 float f;
 
-printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 
-printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
 
-printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
 
-printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
 
  printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 
