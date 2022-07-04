0291ffe49d5aa69b5c53e8d24e8857fa015f4378
yelo94 <akpamib@yahoo.com>
2022-07-04 11:36:56 -0700

    100-intel

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,11 @@
 /*
  *main - entry point
  *
- *Return: 0 after printing the function
+ *Return: always 0 (success)
  */
-int main(void)
+int main(void) 
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
+
 }
