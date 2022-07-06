a89bc686a6b284481a329f0b1b2e9d7e9e7b0add
yelo94 <akpamib@yahoo.com>
2022-07-06 16:24:51 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,20 +6,23 @@
  *0 if the number is zero,
  *-1 if the number is less than zero.
  */
-int print_sign(int n);
+int print_sign(int n)
 {
+int value;
 if (n > 0)
 {
+value = 1;
 _putchar('+');
-return (1);
 }
-else if (n == 0)
+else if (n == 0);
 {
+value = 0;
 _putchar('0');
-return (0);
 }
 else
 {
+value = -1
 _putchar('-');
-return (-1)
+}
+return (value);
 }
