3a17b5b2b178303ba55e276a04d43c102705e0b3
yelo94 <akpamib@yahoo.com>
2022-07-06 20:33:29 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,21 +8,18 @@
  */
 int print_sign(int n)
 {
-int value;
 if (n > 0);
 {
-value = 1;
 _putchar('+');
+return (1);
 }
 else if (n == 0);
 {
-value = 0;
 _putchar('0');
+return (0);
 }
 else
 {
-value = -1;
 _putchar('-');
-}
-return (value);
+return (-1);
 }
