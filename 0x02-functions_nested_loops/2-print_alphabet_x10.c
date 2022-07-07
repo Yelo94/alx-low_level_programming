72230fc7f996a0ded6cf65fb157455f6dae9c942
yelo94 <akpamib@yahoo.com>
2022-07-06 20:06:46 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,8 @@
 /**
  * print_alphabet_x10 -  prints 10 times the alphabet, in lowercase
  * Return :0
- */						
+ */
+int main(void)
  void print_alpahabet_x10(void)
  {
  int count = 0;
@@ -14,6 +15,7 @@ char abc;
  _putchar(abc);
  }
  count++;
+
  _putchar('\n');
  }
 }
