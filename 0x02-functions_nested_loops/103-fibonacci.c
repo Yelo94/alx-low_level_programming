60045cd0e925e9899be06d515aad5323ad5b3b32
yelo94 <akpamib@yahoo.com>
2022-07-06 18:58:12 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -5,22 +5,22 @@
  */
 int main(void)
 {
-	int i;
-	long int fibonacci[50], sum = 2;
-	fibonacci[0] = 1;
-	fibonacci[1] = 2;
+int i;
+long int fibonacci[50], sum = 2;
 
-	for (i = 2; i < 50; i++)
-	{
+fibonacci[0] = 1;
 
-		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+fibonacci[1] = 2;
 
-		if ((fibonacci[i] % 2) == 0 && fibonacci[i] < 4000000)
-			
-			sum += fibonacci[i];
-	}
-	printf("%ld\n", sum);
+for (i = 2; i < 50; i++)
+{
 
-	return (0);
+fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+
+if ((fibonacci[i] % 2) == 0 && fibonacci[i] < 4000000)
+sum += fibonacci[i];
+}
+printf("%ld\n", sum);
+return (0);
 }
 
