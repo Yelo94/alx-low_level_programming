19b7b7bda30eadcff232c4ddc141fed5fb5feb45
yelo94 <akpamib@yahoo.com>
2022-07-12 15:45:56 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,12 +10,14 @@ int c = 0;
 unsigned int ni = 0;
 int min = 1;
 int isi = 0;
+
 while (s[c])
 {
-if  (s[c] == 45)	
+if (s[c] == 45)	
 {
 min *= -1;
 }
+
 while (s[c] >= 48 && s[c] <= 57)
 {
 isi = 1;
--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,5 +1,7 @@
-#include "main.h"
+#include <msth.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  *main - check the code for Holberton School students.
  *Return: Always 0.
