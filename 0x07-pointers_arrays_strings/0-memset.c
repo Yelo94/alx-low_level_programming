4aeb4790c38ceb009b5fb9bd031f03c552a182c2
yelo94 <akpamib@yahoo.com>
2022-07-19 08:32:17 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,12 +7,13 @@
  *@n: The number of bytes to be filled.
  *Return: A pointer to the filled memory area @s.
  */
-void *_memset(void *s, int c, size_t n) 
+void *_memset(void *s, int c, size_t n)
 {
 unsigned int index;
 unsigned char *memory = s, value = c;
 
 for (index = 0; index < n; index++)
-memory[index] = value;
+	memory[index] = value;
+
 return (memory);
 }
