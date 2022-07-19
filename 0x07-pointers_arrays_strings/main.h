692269fd1a78df7cf9ea4f40aa50a4d7499ae509
yelo94 <akpamib@yahoo.com>
2022-07-19 07:24:36 -0700

    .main.h.swp

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,7 +7,7 @@
 /**
  *File: main.h
  *Desc: Header file containing prototypes for all functions
- *used in the 0x06-pointers_arrays_strings directory.
+ *used in the 0x07-pointers_arrays_strings directory.
  */
 
 
