e80504f83138a2d5a20a21dbf1c08d271d16af5f
yelo94 <akpamib@yahoo.com>
2022-07-19 04:07:54 -0700

    1-memcpy.c

--- 0x06-pointers_arrays_strings/1-memcpy.c
+++ 0x06-pointers_arrays_strings/1-memcpy.c
@@ -1,15 +1,16 @@
-#include "holberton.h"
+#include "main.h"
 /**
- *_memcpy - copies memory area
+ *_memcpy - copies n bytes from memory area src
+ * to memory area dest
  *@dest: destination of copy
  *@src: memory area to copy
  *@n: number of bytes to copy
- *Return: pointer to dest
+ *Return: a pointer to @dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 unsigned int i;
-for (i = 0; i < n; i++)	
+for (i = 0; i < n; i++)
 dest[i] = src[i];
 
 return (dest);
