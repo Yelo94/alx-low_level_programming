120a0e3d0d4d8575582707995b91d5599da99445
yelo94 <akpamib@yahoo.com>
2022-07-19 07:31:44 -0700

    main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,21 +1,13 @@
 #ifndef MAIN_H
 
 #define MAIN_H
-
-
-
 /**
- *File: main.h
+ *File: Main.h
+ *Auth: yelo akpami
  *Desc: Header file containing prototypes for all functions
  *used in the 0x07-pointers_arrays_strings directory.
  */
-
-
-
 #include <stddef.h>
-
-
-
 int _putchar(char c);
 
 void *_memset(void *s, int c, size_t n);
