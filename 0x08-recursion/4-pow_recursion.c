dc075156ced624565d7ded7b32ba198b1f7993ed
yelo94 <akpamib@yahoo.com>
2022-07-20 20:08:09 -0700

    101-wildcmp.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,9 +1,10 @@
 #include "main.h"
+
 /**
- * _pow_recursion - returns the value of x raised to the power of y.
- * @x: the number to be raised.
- * @y: the power
- * return: the value of x raised to the power of y.
+ * _pow_recursion - returns the value of the exponentiation
+ *@x: the base
+ *@y: the exponent
+ *_pow_recursion return: value (-1)
  */
 int _pow_recursion(int x, int y)
 {
@@ -13,5 +14,6 @@ return (-1);
 else if (y == 0)
 return (1);
 result *= _pow_recursion(x, y - 1);
-return (result);
+return
+      	(result);
 }
