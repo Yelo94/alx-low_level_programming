cf722587e252532b39dc326431376bbd14d45631
yelo94 <akpamib@yahoo.com>
2022-07-29 19:38:52 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
- *main - Check the code
- *@argc:number of arguments
- *@argv:arguments 
- *Description:If the number of arguments is incorrect or one number 
- *contains non-digits,the function exits with a status of 98.
- *Return: Nothing
+ * main- multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: an array of pointers to the arguments.
+ *
+ * description: if the number of arguments is incorrect or one number
+ *    contains non digits, the function exits with a status of 98.
+ * return: Always 0.
  */
 int main(int argc, char **argv)
 {
-int num1, num2, result;
-if (argc != 3)
-{
-printf("Error\n");
-exit(98);
-}
-else
-{
-num1 = atoi(argv[1]);
-num2 = atoi(argv[2]);
-result = num1 *num2;
-printf("%d\n", result);
-return (0);
-}
+	int num1, num2, result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	else
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		result = num1 * num2;
+		printf("%d\n", result);
+		return (0);
+	}
 }
