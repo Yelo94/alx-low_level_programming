5877875801f90ae889de0c4741db686aa3027134
yelo94 <akpamib@yahoo.com>
2022-08-03 21:57:17 -0700

    3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,40 +1,48 @@
 #include "3-calc.h"
+
 /**
  *op_add - addition of a + b
  *@a: input integer a
  *@b: input integer b
  *Return: sum
  */
+
 int op_add(int a, int b)
 {
 return (a + b);
 }
+
 /**
  *op_sub - subtraction of a - b
  *@a: input integer a
  *@b: input integer b
  *Return: difference
  */
+
 int op_sub(int a, int b)
 {
 return (a - b);
 }
+
 /**
  *op_mul - multiplication of a * b
  *@a: input integer a
  *@b: input integer b
  *Return: product
  */
+
 int op_mul(int a, int b)
 {
 return (a * b);
 }
+
 /**
- *op_div - division of a / b 
- *@a: input integer a
- *@b: input integer b
- *Return: quotient
+ * op_mod - a function that returns the module of a and b
+ * @a: input integer a
+ * @b: input integer b
+ * return: quotient
  */
+
 int op_div(int a, int b)
 {
 if (b != 0)
@@ -44,15 +52,16 @@ exit(100);
 }
 
 /**
- *op_mod - modulo of a / b
- *@a: input integer a
- *@b: input integer b
- *Return: remainder
+ * op_mod - modulo of a / b
+ * @a: input integer a
+ * @b: input integer b
+ * Return: remainder
  */
+
 int op_mod(int a, int b)
 {
-if (b != 0)
-return (a % b);
-printf("Error\n");
-exit(100);
+	if (b != 0)
+		return (a % b);
+	printf("Error\n");
+	exit(100);
 }
