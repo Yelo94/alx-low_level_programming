f52f09d6307750ec6d270f0304779dfb3a26c2dd
Yelo94 <akpamib@yahoo.com>
2022-08-11 16:37:49 -0700

    lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,4 +1,4 @@
-#ifndef _LISTS_H_
+#ifndef _LISTS_H
 #define _LISTS_H_
 #include <stdlib.h>
 #include <string.h>
@@ -9,12 +9,12 @@
  *@len: length of the string
  *@next: points to the next node
  *Description: singly linked list node structure
- *for Bootcamp project
+ *for alx intranet project
  */
 typedef struct list_s
 {
 char *str;
-unsigned int len;			
+unsigned int len;
 struct list_s *next;
 } list_t;
 size_t print_list(const list_t *h);
