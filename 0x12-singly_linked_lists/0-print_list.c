06d69bcedffdf910dfa64c82a4d8878461a3b291
Yelo94 <akpamib@yahoo.com>
2022-08-11 16:11:25 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,9 +6,10 @@
  */
 size_t print_list(const list_t *h)
 {
-unsigned int i = 0;
-const list_t *temp_h = h;
+	unsigned int i = 0;
+	const list_t *temp_h = h;
+
 for (; temp_h; temp_h = temp_h->next, i++)
-printf("[%d] %s\n", temp_h->len, temp_h->str);
+	printf("[%d] %s\n", temp_h->len, temp_h->str);
 return (i);
 }
