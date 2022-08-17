6ae0f89f3f5678afda90961d38e859ca62806810
Yelo94 <akpamib@yahoo.com>
2022-08-16 22:33:41 -0700

    102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -7,27 +7,27 @@
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t i = 0, j;
-	listint_t *temp, *check, *copy = *h;
-	
-	while (*h)
-	{
-		i++;
-		temp = *h;
-		*h = (*h)->next;
-		free(temp);
-		check = copy;
-		j = 0;
-		while (j < i)
-		{
-			if (*h == check)
-			{
-				*h = NULL;
-				return (i);
-			}
-			check = check->next;
-			j++;
-		}
-	}
-	return (i);
+size_t i = 0, j;
+listint_t *temp, *check, *copy = *h;
+
+while (*h)
+{
+i++;
+temp = *h;
+*h = (*h)->next;
+free(temp);
+check = copy;
+j = 0;
+while (j < i)
+{
+if (*h == check)
+{
+*h = NULL;
+return (i);
+}
+check = check->next;
+j++;
+}
+}
+return (i);
 }
