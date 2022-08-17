0aee0c1997872efad26d5dbe35d6d11bb05621e4
Yelo94 <akpamib@yahoo.com>
2022-08-17 16:56:14 -0700

    102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,33 +1,35 @@
 #include "lists.h"
-
+#include <stdlib.h>
 /**
- *free_listint_safe - frees linked list safe version
- *@h: input head of linked list
- *Return: size of list that was freed
- */
+**free_listint_safe - a function that frees a list
+**@h: input head of linked list
+**return: size of list that was freed
+*/
 size_t free_listint_safe(listint_t **h)
 {
-size_t i = 0, j;
-listint_t *temp, *check, *copy = *h;
+	size_t i = 0, j;
+	listint_t *temp, *check, *copy = *h;
 
-while (*h)
-{
-i++;
-temp = *h;
-*h = (*h)->next;
-free(temp);
-check = copy;
-j = 0;
-while (j < i)
-{
-if (*h == check)
-{
-*h = NULL;
-return (i);
+	while (*h)
+	{
+		i++;
+		temp = *h;
+		*h = (*h)->next;
+		free(temp);
+		check = copy;
+		j = 0;
+		while (j < i)
+		{
+			if (*h == check)
+			{
+				*h = NULL;
+				return (i);
+			}
+			check = check->next;
+			j++;
+		}
+	}
+	return (i);
 }
-check = check->next;
-j++;
-}
-}
-return (i);
-}
+
+
