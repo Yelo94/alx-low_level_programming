6040ac8fff074fb236673fc004bc6719417a12cb
Yelo94 <akpamib@yahoo.com>
2022-08-17 21:01:01 -0700

    102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,85 +1,36 @@
-t;                                                              
-hare = (hare->next)->next;                                      }        
-return (0);
-}
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
 **free_listint_safe - a function that frees a listint_t list safely (ie.
 **can free lists containing loops)
 **@h: A pointer to the address of
-**the head of the listint_t list.
-**
+**the head of the listint_t list
 **return: size of list that was freed
 **Description: The function sets the head to NULL.
 **/
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *tmp;
-	size_t nodes, index;
-	
-	nodes = looped_listint_count(*h);
-
-	if(nodes == 0)
-	{
-		for (; h != NULL && *h !=NULL; nodes++)
-		{
-			tmp = (*h)->next;
-			free(*h);
-			*h = tmp;
-		}
-	}
-
-	else
-	{
-		for (index = 0; index < nodes; index++)
-		{
-			tmp = (*h)->next;
-			free(*h);
-			*h = tmp;
-		}
-		*h = NULL;
-
-		return (nodes);
-	}
-
-#include "lists.h"
-
-size_t looped_listint_count(listint_t *head);
-size_t free_listint_safe(listint_t**h);
-
-/**
- * *looped_listint - counts the number of unique nodes
- *                  in a looped listint_t linked list.
- *  *@head: pointer to the head of the listint_t to check. 
- *  *
- *  *return if the list is not looped - 0
- *     otherwise - the number of unique nodes in the list.
- * */
-size_t looped_listint_count(listint_t *head)
+size_t i = 0, j;
+listint_t *temp, *check, *copy = *h;
+while (*h)
 {
-	listint_t *tortoise, *hare;
-	size_t nodes = 1;
-
-	if (head == NULL || head->next == NULL)
-		return (0);
-	tortoise = head->next;
-	hare = (head->next)->next;
-	while (hare)
-	{
-		if (tortoise == hare)
-		{
-			tortoise = head;
-			while (tortoise != hare)
-			{
-				nodes++;
-				tortoise = tortoise->next;
-				hare = hare->next;
-			}
-			tortoise = tortoise->next;
-			while (tortoise != hare)
-			{
-				nodes++;
-				tortoise = tortoise->next;
-			}
-			return (nodes);
-		}
-		tortoise = tortoise->nex
+i++;
+temp = *h;
+*h = (*h)->next;
+free(temp);
+check = copy;
+j = 0;
+while (j < i)
+{
+if (*h == check)
+{
+*h = NULL;
+return (i);
+}
+check = check->next;
+j++;
+}
+}
+return (i);
+}
