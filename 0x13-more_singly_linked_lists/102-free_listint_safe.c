eef6713917b7aeb465e83c4fc78917a5873be8b8
Yelo94 <akpamib@yahoo.com>
2022-08-17 17:49:21 -0700

    lists.h

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,35 +1,49 @@
+#include <stdio.h>
 #include "lists.h"
 #include <stdlib.h>
+
 /**
-**free_listint_safe - a function that frees a list
-**@h: input head of linked list
+**free_listint_safe - a function that frees a listint_t list safely (ie.
+**can free lists containing loops)
+**@h: A pointer to the address of
+**the head of the listint_t list.
+**
 **return: size of list that was freed
-*/
+**Description: The function sets the head to NULL.
+**/
 size_t free_listint_safe(listint_t **h)
 {
-	size_t i = 0, j;
-	listint_t *temp, *check, *copy = *h;
+	size_t counter = 0;
+	listint_t *temp;
 
-	while (*h)
+	temp = *h;
+	while (temp)
 	{
-		i++;
 		temp = *h;
-		*h = (*h)->next;
-		free(temp);
-		check = copy;
-		j = 0;
-		while (j < i)
-		{
-			if (*h == check)
-			{
-				*h = NULL;
-				return (i);
-			}
-			check = check->next;
-			j++;
-		}
+		temp = temp->next;
+		free_list(temp);
+		counter++;
 	}
-	return (i);
+	*h = NULL;
+
+	return (counter);
 }
 
+/**
+ * free_list - A function that frees a listint_t recursivel
+ * @head: A pointer to the listint_t structure
+ * return: nothing
+ */
+void free_list(listint_t *head)
+{
+	listint_t *temp;
 
+	if (head)
+	{
+		temp = head;
+		temp = temp->next;
+		free(temp);
+		free_list(temp);
+	}
+	free(head);
+}
