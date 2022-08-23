d178c272792ee5cdc936742479c059c16b34e589
Yelo94 <akpamib@yahoo.com>
2022-08-23 07:59:11 -0700

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  *main - The entry point for program to get header of ELF file
  *@argc: The number of arguments
@@ -7,6 +8,6 @@
  */
 int main(int argc, char *argv[])
 {
-printf("argc:%d, argv:%p\n", argc, (void *)*argv);			
-return (1);
+	printf("argc:%d, argv:%p\n", argc, (void *)*argv);
+	return (1);
 }
